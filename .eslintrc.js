61dc4450bda5f34bbf0933b2786ffae338c0fae8
Samppa Salo <samppasalo16@gmail.com>
2025-03-31 20:34:56 +0300

    modify eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,8 +12,8 @@ module.exports = {
     "ecmaFeatures": {
       "jsx": true
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    "ecmaVersion": "latest",
+    "sourceType": "commonjs"
   },
   "plugins": [
     "react", "jest"
